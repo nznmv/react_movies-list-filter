d5a4a5681925721155716e12ab24f5bde137dc46
nznmvs <nznmvs@gmail.com>
2020-09-08 09:46:42 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.f07ec09265767684563bd605b35376eb.js"
 );
 
 self.addEventListener('message', (event) => {
